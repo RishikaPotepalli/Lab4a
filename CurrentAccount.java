aac397994bf51eeb2ddd60bef0aa7c3b567c0102
RishikaPotepalli <82092914+RishikaPotepalli@users.noreply.github.com>
2021-04-14 13:23:06 +0530

    Add files via upload

---
+++ CurrentAccount.java
@@ -0,0 +1,30 @@
+package lab4;
+
+public class CurrentAccount extends Account{
+
+       private double overDraftLimit;
+
+	public double getOverDraftLimit() {
+		return overDraftLimit;
+	}
+
+	public void setOverDraftLimit(double overDraftLimit) {
+		this.overDraftLimit = overDraftLimit;
+	}
+
+	public CurrentAccount() {
+		super();
+	}
+
+	@Override
+	public void withDraw(double amount) {
+		// TODO Auto-generated method stub
+		if((super.getBalance() + amount) > overDraftLimit) {
+			System.out.println("Over Draft Limit has been exceeded.");
+		}
+		//return true or false
+	}
+       
+}
+
+}
